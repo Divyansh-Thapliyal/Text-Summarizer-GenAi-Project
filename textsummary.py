4581b2f58372e11996fd1e6a5e6f8da589df7b7f
Divyansh Thapliyal <divyanshthapliyalwork007@gmail.com>
2025-08-07 19:47:16 +0530

    Gradio functionality added

--- textsummary.py
+++ textsummary.py
@@ -7,6 +7,18 @@ from transformers import pipeline
 
 text_summary = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
-text="The quick brown fox jumps over the lazy dog. " * 10
+# text="The quick brown fox jumps over the lazy dog. " * 10
+# print(text_summary(text))
 
-print(text_summary(text))
+#till here it is only console based. Lets use gradio to give it a GUI (graphic user interface) and make it a web app....
+
+#Lets make a function bcz gradio needs a function to call.
+
+def summary(input):
+    output = text_summary(input)
+    return output[0]['summary_text']
+
+gr.close_all()
+
+demo = gr.Interface(fn=summary, inputs="text", outputs="text")
+demo.launch()
